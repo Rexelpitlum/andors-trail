fae1188ce8a94d57a31ca2d17f020d52bbae7830
Rexelpitlum <Not.a.mailaddress@nowhere.com>
2021-12-27 23:18:20 +0100

    Added legendary and extraordinary items to all droplists, each with a droprate of 1/10000

--- AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/parsers/DropListParser.java
+++ AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/parsers/DropListParser.java
@@ -12,6 +12,8 @@ import com.gpl.rpg.AndorsTrail.resource.parsers.json.JsonCollectionParserFor;
 import com.gpl.rpg.AndorsTrail.resource.parsers.json.JsonFieldNames;
 import com.gpl.rpg.AndorsTrail.util.L;
 import com.gpl.rpg.AndorsTrail.util.Pair;
+import org.json.JSONArray;
+
 
 public final class DropListParser extends JsonCollectionParserFor<DropList> {
 
@@ -33,7 +35,134 @@ public final class DropListParser extends JsonCollectionParserFor<DropList> {
 	@Override
 	protected Pair<String, DropList> parseObject(JSONObject o) throws JSONException {
 		String droplistID = o.getString(JsonFieldNames.DropList.dropListID);
-		DropItem[] items = dropItemParser.parseArray(o.getJSONArray(JsonFieldNames.DropList.items));
+		DropItem[] standardItems = dropItemParser.parseArray(o.getJSONArray(JsonFieldNames.DropList.items));
+		String specialItemsJson="[\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"ring_shadow0\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"hunters_sword\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"rapier_lifesteal\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"elytharan_redeemer\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"Helm_foreseeing\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"yczorah2\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"gloves_life\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"shield_blk_defender\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"whip_bind\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"shadowfang\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"chaosreaper\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"valugha_gown\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"valugha_hat\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"clmr_wrmas\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t},\n" +
+				"\t{\n" +
+				"\t\t\"itemID\":\"gem_fire\",\n" +
+				"\t\t\"chance\":\"1/10000\",\n" +
+				"\t\t\"quantity\":{\n" +
+				"\t\t\t\"min\":1,\n" +
+				"\t\t\t\"max\":1\n" +
+				"\t\t}\n" +
+				"\t}\n" +
+				"]";
+		JSONArray specialItemsJsonArray= new JSONArray(specialItemsJson);
+		DropItem[] specialItems= dropItemParser.parseArray(specialItemsJsonArray);
+		DropItem[] items= new DropItem[standardItems.length + specialItems.length];
+		System.arraycopy(standardItems,0,items,0,standardItems.length);
+		System.arraycopy(specialItems,0,items,standardItems.length,specialItems.length);
 
 		if (AndorsTrailApplication.DEVELOPMENT_VALIDATEDATA) {
 			if (items == null) {
